d3d38a9d662beaeca08db8ae357a82405b70ae30
Mariusz <mariuszbia@yahoo.com>
2022-05-09 00:35:24 -0500

    Completed assignment

--- main.js
+++ main.js
@@ -3,9 +3,46 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', () => {
+  const heartUIs = document.getElementsByClassName('like-glyph');
+  for(let i=0; i<heartUIs.length; i++) {
+    registerHeartUiClicks(heartUIs[i]);
+  }
+})
 
+function registerHeartUiClicks(heartUI) {
+  heartUI.addEventListener('click', handleHeartUiClick);
+}
 
+async function handleHeartUiClick(event) {
+  event.preventDefault();
+  mimicServerCall()
+  .then(changeHeart(event))
+  .catch(handleHeartClickError);
+}
 
+function changeHeart(event) {
+    const heart = event.target;
+    if(heart.innerText === EMPTY_HEART) {
+      heart.innerText = FULL_HEART;
+      heart.setAttribute('class', 'activated-heart');
+    }
+    else {
+      heart.innerText = EMPTY_HEART;
+      heart.removeAttribute('class');
+    }
+}
+
+function handleHeartClickError(error) {
+  document.getElementById('modal').removeAttribute('class');
+  document.getElementById('modal-message').innerText = error;
+  const ERROR_DISPLAY_MILISECONDS = 3000;
+  const errorDisplayTimeout = setTimeout(hideErrorDisplay, ERROR_DISPLAY_MILISECONDS);
+}
+
+function hideErrorDisplay() {
+  document.getElementById('modal').setAttribute('class', 'hidden');
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
